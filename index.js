dfb338432169e64c3285877b8a0365e3a062e630
adhe7x <105721068+adhe7x@users.noreply.github.com>
2022-05-31 23:21:20 +0700

    Edited by Adhi Prasetyo

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 // ! JANGAN DIMOFIDIKASI
 const dataBelanjaan = [
-  {
-    id: "D-10001",
-    nama: "Minyak Goreng Delima",
-    harga: 25000,
-    kuantitas: 2,
-  },
-  {
-    id: "D-10020",
-    nama: "Beras Mamos",
-    harga: 48000,
-    kuantitas: 1,
-  },
-  {
-    id: "F-00010",
-    nama: "Larutan Cap Kaki Empat",
-    harga: 7500,
-    kuantitas: 8,
-  },
-];
+    {
+      id: "D-10001",
+      nama: "Minyak Goreng Delima",
+      harga: 25000,
+      kuantitas: 2,
+    },
+    {
+      id: "D-10020",
+      nama: "Beras Mamos",
+      harga: 48000,
+      kuantitas: 1,
+    },
+    {
+      id: "F-00010",
+      nama: "Larutan Cap Kaki Empat",
+      harga: 7500,
+      kuantitas: 8,
+    },
+  ];
+  
+  // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
 
-// boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+let list = [];
+let total = null;
 
-// boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+const listBelanjaan = (belanja) => {
+    for (x = 0; x < belanja.length; x++) {
+        list.push(`- ${belanja[x].nama} x ${belanja[x].kuantitas}`);
+    };
+    return list;
+};
+  
+  // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
 
-// ! JANGAN DIMODIFIKASI
-const main = () => {
-  console.log("Selamat datang customer");
-
-  console.log("Belanjaan Anda adalah:");
-  console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
-  console.log(
-    "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
-  );
+function totalBelanjaan(belanja) {
+    for (x = 0; x < belanja.length; x++) {
+        total += (belanja[x].harga * belanja[x].kuantitas);
+    };
+    return total;
 };
 
-main();
-
-module.exports = {
-  dataBelanjaan,
-  listBelanjaan,
-  totalBelanjaan,
-};
+  // ! JANGAN DIMODIFIKASI
+  const main = () => {
+    console.log("Selamat datang customer");
+  
+    console.log("Belanjaan Anda adalah:");
+    console.log(listBelanjaan?.(dataBelanjaan)?.join("\n"));
+    console.log(
+      "\nTotal Belanjaan Anda adalah Rp. " + totalBelanjaan?.(dataBelanjaan)
+    );
+  };
+  
+  main();
+  
+  module.exports = {
+    dataBelanjaan,
+    listBelanjaan,
+    totalBelanjaan,
+  };
